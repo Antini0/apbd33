395b2a7cb2c5fbf08a8a487b9afb65ce56efe44b
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-03-22 12:23:41 +0100

    mod container ship and tests

--- apbd3/apbd3/ContainerShip.cs
+++ apbd3/apbd3/ContainerShip.cs
@@ -1,6 +1,116 @@
-﻿namespace apbd3;
+﻿using apbd3.Containers;
+
+namespace apbd3;
 
 public class ContainerShip
 {
+    public List<container> listOfContainers = new List<container>();
+    public double maxSpeed { get; set; }
+    public double maxCombinedMass { get; set; }
+    public int containerLimit { get; set; }
+    public double currentMass = 0;
+    public bool massOk = true;
+
+    public ContainerShip(double maxSpeed, double maxCombinedMass, int containerLimit)
+    {
+        this.maxSpeed = maxSpeed;
+        this.maxCombinedMass = maxCombinedMass;
+        this.containerLimit = containerLimit;
+    }
+
+    public void addContainer(container c)
+    {
+        checkMass(c.cargoMass);
+        if (massOk)
+        {
+            listOfContainers.Add(c);
+            currentMass += c.cargoMass + c.ownMass;
+        }
+    }
     
+    public void addMultipleContainers(List<container> list)
+    {
+        double mass = 0;
+        
+        foreach (var c in list)
+        {
+            mass += c.cargoMass + c.ownMass;
+        }
+        
+        checkMass(mass);
+
+        if (massOk)
+        {
+            foreach (var c in list)
+            {
+                listOfContainers.Add(c);
+                currentMass += c.cargoMass + c.ownMass;
+            } 
+        }
+    }
+    public void removeContainer(container c)
+    {
+        if (listOfContainers.Contains(c))
+        {
+            listOfContainers.Remove(c);
+            currentMass -= c.cargoMass + c.ownMass;
+        }
+    }
+    
+    public void unloadShip()
+    {
+        listOfContainers.Clear();
+        currentMass = 0;
+    }
+    
+    public void swapContainers(container c1, string serialNum)
+    {
+        List<int> indexesToRemove = new List<int>();
+
+        for (int i = 0; i < listOfContainers.Count; i++)
+        {
+            if (listOfContainers[i].serialNum == serialNum)
+            {
+                indexesToRemove.Add(i);
+            }
+        }
+
+        foreach (int index in indexesToRemove)
+        {
+            currentMass -= listOfContainers[index].cargoMass + listOfContainers[index].ownMass;
+            listOfContainers.RemoveAt(index);
+        }
+
+        if (indexesToRemove.Count > 0)
+        {
+            int lastIndex = indexesToRemove[indexesToRemove.Count - 1];
+            checkMass(c1.cargoMass);
+            if (massOk)
+            {
+                listOfContainers.Insert(lastIndex, c1);
+                currentMass += c1.cargoMass + c1.ownMass;
+            }
+        }
+    }
+
+    public void checkMass(double addedMass)
+    {
+        if (currentMass + addedMass > maxCombinedMass)
+        {
+            massOk = false;
+            throw new OverfillExcetion("maksymalna masa ładunku przekroczona");
+        }
+        
+    }
+    
+    public override string ToString()
+    {
+        string containerInfo = "";
+        foreach (container container in listOfContainers)
+        {
+            containerInfo += container.ToString() + "\n";
+        }
+
+        return $"Container Ship:\nMax Speed: {maxSpeed}\nMax Combined Mass: {maxCombinedMass}\nContainer Limit: {containerLimit}\nContainers:\n{containerInfo}Current Mass: {currentMass}";
+    }
 }
--- apbd3/apbd3/Program.cs
+++ apbd3/apbd3/Program.cs
@@ -3,23 +3,45 @@
 using apbd3;
 using apbd3.Containers;
 
-//var container = new container(); 
-int? a = 2;
-a = null;
+void transferContainer(container c,ContainerShip originalShip, ContainerShip designatedShip)
+{
+    originalShip.removeContainer(c);
+    designatedShip.addContainer(c);
+}
 
-//map
+void getContainerInfo(container c)
+{
+    Console.WriteLine(c.ToString());
+}
 
+void getShipInfo(ContainerShip cs)
+{
+    Console.WriteLine(cs.ToString());
+}
+
+List<container> list = new List<container>();
 
 var container = new LiquidContainer(20.0, 5.0, 2.0, 4.75, 40.0, true);
 var container2 = new LiquidContainer(27.0, 4.0, 2.0, 4.75, 40.0, false);
 var container3 = new GasContainer(27.0, 4.0, 2.0, 4.75, 40.0, 3);
-var container4 = new CoolingContainer(12.0, 4.0, 2.0, 7.75, 35.0, PossibleProducts.FISH);
-Console.WriteLine(container.ToString());
-Console.WriteLine(container3.ToString());
-Console.WriteLine(container4.ToString());
+var container4 = new GasContainer(27.0, 4.0, 2.0, 4.75, 40.0, 3);
+var container5 = new GasContainer(27.0, 4.0, 2.0, 4.75, 40.0, 3);
+var container6 = new CoolingContainer(12.0, 4.0, 2.0, 7.75, 35.0, PossibleProducts.FISH);
 
-container2.Load(10);
-container3.unload(20);
 
-Console.WriteLine(container3.ToString());
-Console.WriteLine("\n"+container2.ToString());
+list.Add(container2);
+list.Add(container3);
+list.Add(container4);
+list.Add(container5);
+
+var cs1 = new ContainerShip(30, 500, 20);
+
+cs1.addContainer(container);
+
+cs1.addMultipleContainers(list);
+
+cs1.swapContainers(container6, "KON-G-3");
+
+getShipInfo(cs1);
+
+
